49c01035cefde90c8f559493197afc17742d0dfe
krishna13579 <mkm.mouli@gmail.com>
2015-04-08 17:48:14 +0530

    initial project version

---
+++ Sample_JWT_Token/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sample_JWT_Token.WebApiApplication" Language="C#" %>
---
+++ sample/Scripts/common.js
@@ -0,0 +1,27 @@
+﻿var endpoints = {
+    ServiceEndpointUrl:  "ServiceHost",
+    IdpOauthEndpointUrl: "https://localhost/OSCid/issue/oauth2/authorize"
+};
+    var oAuthConfig = {
+        client_id: "Sample_Client",
+        scope: "urn:Sample",
+        response_type: "token",
+        redirect_uri: "https://localhost/Sample/home.html"
+    }
+
+
+    if (!Array.prototype.indexOf) {
+        Array.prototype.indexOf = function (obj, start) {
+            for (var i = (start || 0), j = this.length; i < j; i++) {
+                if (this[i] === obj) { return i; }
+            }
+            return -1;
+        }
+    }
+
+$.ajaxPrefilter(function (options) {
+    options.headers = { 'Authorization': 'Bearer ' + readCookie('CCAuth') };
+});
+
+
+
